5afd18477fcab5bf88d146f400fbec633ac7756d
theph03*nix215 <wannabecoastie@gmail.com>
2023-05-23 11:50:25 -0400

    Update Seeds.js
    @vanessa-cooper

--- backend/scripts/seeds.js
+++ backend/scripts/seeds.js
@@ -1,1 +1,18 @@
 //TODO: seeds script should come here, so we'll be able to put some data in our local env
+const mongoose = require('mongoose');
+const Item = require('../models/Item')
+const Comment = require('../models/Comment')
+const User = require('../models/User');
+
+let itemArray =[];
+let commentArray=[
+    new Comment({
+        body:"Some text for a comment",
+        seller:"3",
+        item:"1"
+    })
+];
+let userArray=[User({
+
+})]
+ 
