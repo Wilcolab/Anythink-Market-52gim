d38989866335c1474df93f86faa5d0fb1ee5a6f9
theph03*nix215 <wannabecoastie@gmail.com>
2023-05-23 14:07:55 -0400

    Update Seeds.js
    @vanessa-cooper

--- backend/scripts/seeds.js
+++ backend/scripts/seeds.js
@@ -1,18 +1,65 @@
 //TODO: seeds script should come here, so we'll be able to put some data in our local env
 const mongoose = require('mongoose');
-const Item = require('../models/Item')
-const Comment = require('../models/Comment')
-const User = require('../models/User');
+require('./../models/User')
+require('../models/Item')
+require('../models/Comment')
 
-let itemArray =[];
-let commentArray=[
-    new Comment({
-        body:"Some text for a comment",
-        seller:"3",
-        item:"1"
-    })
-];
-let userArray=[User({
+//var Item = mongoose.model('Item')
+var Comment = mongoose.model('Comment')
+var User = mongoose.model('User');
+var Item = mongoose.model('Item')
 
-})]
- 
+let userId;
+let itemId;
+
+async function seedDatabase(){
+    
+ const users = Array.from(Array(1)).map((_item,i)=>({
+    username:`trueuser${i}`,
+    email:`trueuser${i}@anythinks.com`,
+    bio: `test id bio thingy:${i+1}`,
+    image:'https://picsum.photos/200',
+    role:'user',
+    favorites:[],
+    following:[]
+     }));
+for(let user of users){
+    const u = new User(user);
+    const dbItem = await u.save();
+    if(!itemId){
+        itemId = dbItem._id;
+    }
+    
+}
+const items = Array.from(Array(1)).map((_item,i)=>({
+    slug:`fakeitem${i}`,
+    title:`Fake Item Title ${i}`,
+    description:`Fake description ${i}`,
+    image:'https://picsum.photos/200',
+    comments:[],
+    tagList:['tag','item'],
+    seller:userId
+
+}));
+for(item of items){
+    const it = new Item(item)
+    const  dbItem = await it.save()
+    if(!itemId){
+        itemId = dbItem._id;
+    }
+}
+
+const comments = Array.from(Array(1)).map((_item,id)=>({
+    body:'This is a test body',
+    seller:userId,
+    item:itemId
+}));
+for(comment of comments){
+    const c =new Comment(comment);
+    await c.save();
+}
+}
+
+seedDatabase().then(()=>{
+    process.exit();
+})
